31e69130e59453727184796633c9d312f1c3bac6
sampreethD <45105475+sampreethD@users.noreply.github.com>
2020-12-09 20:29:23 +0530

    lesson 2: Use of props  Use of props in functional component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import React from "react";
 import "./styles.css";
 
 //lesson no.1 - Function name should start with caps if you are using as a component
-export default function App() {
+export default function App(props) {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>Hello React - Use of props in functional component</h1>
+      <h2>This is props : {props.property}, passed to this function</h2>
     </div>
   );
 }
