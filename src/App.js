e58f04960581cbb6eb34221baf89f7acc3605ba0
sampreethD <45105475+sampreethD@users.noreply.github.com>
2020-12-09 20:21:58 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./styles.css";
 
+//lesson no.1 - Function name should start with caps if you are using as a component
 export default function App() {
   return (
     <div className="App">
